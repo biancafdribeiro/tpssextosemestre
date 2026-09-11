c61a140729588045f5d0944c3c7626a19f84af37
biancafdribeiro <biancafdribeiro@gmail.com>
2026-09-11 00:34:49 -0300

    TP 01 - projeto MAUI

---
+++ cbtprdm/tp01/MauiApp3/MauiApp3/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿namespace MauiApp3;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void BtnOk_Clicked(object sender, EventArgs e)
+    {
+        if (txtId.Text == "admin" && txtSenha.Text == "senha@dmin")
+        {
+            await DisplayAlert(
+                "Login",
+                "Login realizado com sucesso!",
+                "OK");
+        }
+        else
+        {
+            await DisplayAlert(
+                "Login",
+                "Login não autorizado.",
+                "OK");
+        }
+    }
+
+    private void BtnLimpar_Clicked(object sender, EventArgs e)
+    {
+        txtId.Text = "";
+        txtSenha.Text = "";
+
+        txtId.Focus();
+    }
+
+    private async void BtnCreditos_Clicked(object sender, EventArgs e)
+    {
+        await DisplayAlert(
+            "Créditos",
+            "Autora do aplicativo: Bianca Fonseca Dantas Ribeiro - CB3025683",
+            "OK");
+    }
+}
